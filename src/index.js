276c3683d3735a0ea92edf9a0949b0508dfd2219
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-15 17:32:43 +0530

    Updated some components on index js file

--- src/index.js
+++ src/index.js
@@ -121,15 +121,18 @@ class Game extends React.Component
 
         return (
             <div className="game">
-                <div className="game-board">
-                    <Board
-                        squares={current.squares}
-                        onClick={(i) => this.handleClick(i)}
-                    />
-                </div>
-                <div className="game-info">
-                <div>{status}</div>
-                <ol>{moves}</ol>
+                <div className="game-title">Tic Tac Toe</div>
+                <div className="game-box">
+                    <div className="game-board">
+                        <Board
+                            squares={current.squares}
+                            onClick={(i) => this.handleClick(i)}
+                        />
+                    </div>
+                    <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                    </div>
                 </div>
             </div>
         );
