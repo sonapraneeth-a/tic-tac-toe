84e0cef5b431a0597c8a1de2ef3b6dd1e49b2800
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-18 22:10:38 +0530

    Updated minor version of the app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Game from './Game';
 ReactDOM.render (
     <Game
         title="Tic Tac Toe"
-        version="0.5.0"
+        version="0.6.0"
     />,
     document.getElementById('app')
 );
