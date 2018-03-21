97042fbfb69043c24ac0240a9d3d0976ad79fbe3
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-21 15:47:34 +0530

    Bumped version to 0.7.0

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Game from './Game';
 ReactDOM.render (
     <Game
         title="Tic Tac Toe"
-        version="0.6.2"
+        version="0.7.0"
     />,
     document.getElementById('app')
 );
